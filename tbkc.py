d54f9b98a61a8bfa3ceda39209c56fc4cdcb7faf
Jessica Emberly <jessicaemberly1+github@gmail.com>
2026-07-19 16:17:19 -0500

    Add files via upload

--- tbkc.py
+++ tbkc.py
@@ -2,8 +2,10 @@ import os
 import requests
 os.system("clear")
 key = input("What is your Text Belt key?: ")
+os.system("clear")
+phonenumber = input("What is your phone number?: ")
 response = requests.post('https://textbelt.com/text', {
-      'phone': '',
+      'phone': phonenumber,
       'message': 'test',
       'key': key,
     })
